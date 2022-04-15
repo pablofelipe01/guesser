ab1a939db8a55d95a3e80be85bff678c6d4c69e3
Pablo Acebedo <pablofelipe@me.com>
2022-04-15 10:41:28 -0500

    Functions

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ let currentRoundNumber = 1;
 const generateTarget = (number) => {
     return Math.floor(Math.random() * 10);
 }
+
+const compareGuesses = (human, computer, target) => {
+
+    const humanDifference = Math.abs(target-human)
+    const computerDifference = Math.abs(target-computer)
+
+    if (humanDifference <= computerDifference) {
+      return true 
+    } else {
+      return false
+    }
+}
+
+const updateScore = winner => {
+    if (winner === 'human') {
+      humanScore++
+    } else if (winner === 'human') {
+      computerScore++
+    } else {
+      console.log (error)
+    }
+  
+  }
+
+  const advanceRound = () => 
+   currentRoundNumber++
